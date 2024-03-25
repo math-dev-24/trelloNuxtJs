e13de237de23d553270e2b2432679ee7adfb6fc3
Mathieu <mathieu.busse@live.fr>
2024-03-25 15:55:31 +0100

    add created date task

--- components/OptionTask.vue
+++ components/OptionTask.vue
@@ -38,9 +38,15 @@ function actualy_state(){
       </select>
       <input @keyup.enter="emit('close')" type="text" v-model="task.title" class="rounded px-2 py-1 text-xl bg-slate-100 focus:bg-slate-100">
       <textarea v-model="task.content" class="bg-slate-100" />
-      <button @click="emit('delete')" class="p-1 mt-2 hover:text-red-500 hover:bg-slate-100 p-1 rounded w-[30px] transition duration-200">
-        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1zm1 2H6v12h12zm-9 3h2v6H9zm4 0h2v6h-2zM9 4v2h6V4z"/></svg>
-      </button>
+      <div class="flex flex-row items-center">
+        <button @click="emit('delete')" class="p-1 mt-2 hover:text-red-500 hover:bg-slate-100 p-1 rounded w-[30px] transition duration-200">
+          <svg xmlns="http://www.w3.org/2000/svg" width="22" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1zm1 2H6v12h12zm-9 3h2v6H9zm4 0h2v6h-2zM9 4v2h6V4z"/></svg>
+        </button>
+        <div class="text-xs flex-1 text-end">
+          <p>Créer le</p>
+          <p>{{task.createdAt.toLocaleString("fr-FR")}}</p>
+        </div>
+      </div>
     </div>
     <div @click="emit('close')" class="absolute top-0 left-0 z-0 h-full w-full bg-slate-300 opacity-60"></div>
   </teleport>
